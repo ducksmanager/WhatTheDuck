2517b778979d2c6e7820fd4638eded71c1cfa114
Bruno Perel <brunoperel@gmail.com>
2016-07-11 20:54:12 +0200

    Use switch at the bottom of the lists to change list type

--- app/src/main/java/net/ducksmanager/whattheduck/List.java
+++ app/src/main/java/net/ducksmanager/whattheduck/List.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import net.ducksmanager.whattheduck.Collection.CollectionType;
@@ -23,19 +24,19 @@ public abstract class List extends ListActivity{
     protected static final int INSERT_ID = 1;
     private static final int MY_COLLECTION_ID = 2;
     private static final int LOGOUT = 3;
-    
+
     public String type;
     private ArrayList<String> items;
-    
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         Bundle extras = getIntent().getExtras();
-		type = extras != null && extras.getString("type") != null 
-					? extras.getString("type") 
+		type = extras != null && extras.getString("type") != null
+					? extras.getString("type")
 					: CollectionType.USER.toString();
-		
+
 		setContentView(R.layout.wtd_list);
 
 		this.findViewById(R.id.navigationAllCountries).setOnClickListener(new View.OnClickListener() {
@@ -58,6 +59,21 @@ public abstract class List extends ListActivity{
 				goToView(IssueList.class);
 			}
 		});
+
+		Switch onlyInCollectionSwitch = (Switch) this.findViewById(R.id.onlyInCollectionSwitch);
+		onlyInCollectionSwitch.setChecked(type.equals(CollectionType.USER.toString()));
+
+		onlyInCollectionSwitch.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Switch onlyInCollectionSwitch = (Switch) view;
+				List.this.goToAlternativeView(
+					onlyInCollectionSwitch.isChecked()
+						? CollectionType.USER.toString()
+						: CollectionType.COA.toString()
+				);
+			}
+		});
     }
 
 	private void goToView(Class<?> cls) {
@@ -68,8 +84,14 @@ public abstract class List extends ListActivity{
 		}
 	}
 
+	private void goToAlternativeView(String collectionType) {
+		Intent i = new Intent(WhatTheDuck.wtd, this.getClass());
+		i.putExtra("type", collectionType);
+		startActivity(i);
+	}
+
     public abstract void show();
-    
+
     public void show(ArrayList<String> items) {
     	this.items = items;
 
@@ -77,15 +99,15 @@ public abstract class List extends ListActivity{
             TextView emptyListText = (TextView) this.findViewById(R.id.emptyList);
             emptyListText.setVisibility(TextView.VISIBLE);
         }
-    	
+
     	String[] lv_arr = items.toArray(new String[items.size()]);
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lv_arr));
-        
+
     	if (items.size() > 20) {
     		EditText filterEditText = (EditText) this.findViewById(R.id.filter);
     		filterEditText.setVisibility(EditText.VISIBLE);
     		filterEditText.requestFocus();
-    		
+
     		filterEditText.addTextChangedListener(new TextWatcher() {
                 public void afterTextChanged(Editable s) { }
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
@@ -95,10 +117,10 @@ public abstract class List extends ListActivity{
                 	for (String item : List.this.items)
                 		if (item.replace("* ", "").toLowerCase(Locale.FRANCE).contains(typedText.toLowerCase()))
                 			filteredItems.add(item);
-                	
+
                 	String[] lv_arr = filteredItems.toArray(new String[filteredItems.size()]);
                     setListAdapter(new ArrayAdapter<String>(List.this, android.R.layout.simple_list_item_1, lv_arr));
-                    
+
     			}
             });
     	}
@@ -111,8 +133,8 @@ public abstract class List extends ListActivity{
         	menu.add(0, INSERT_ID, 0, R.string.insert_issue_menu);
         else
         	menu.add(0, MY_COLLECTION_ID, 0, R.string.my_collection_menu);
-        menu.add(0, LOGOUT, 1, R.string.logout_menu);	
-        
+        menu.add(0, LOGOUT, 1, R.string.logout_menu);
+
         return true;
     }
 
@@ -120,7 +142,7 @@ public abstract class List extends ListActivity{
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
     	return onMenuItemSelected(featureId, item, false);
     }
-    
+
     public boolean onMenuItemSelected(int featureId, MenuItem item, boolean alreadyDone) {
     	if (!alreadyDone) {
     		Intent i = null;
@@ -137,19 +159,19 @@ public abstract class List extends ListActivity{
 	    	        i = new Intent(WhatTheDuck.wtd, WhatTheDuck.class);
 	            	WhatTheDuck.userCollection = new Collection();
 	            	WhatTheDuck.coaCollection = new Collection();
-	            	
+
 	            break;
 	        }
 	        startActivity(i);
     	}
-    	
+
         return super.onMenuItemSelected(featureId, item);
     }
-    
+
     protected Collection getCollection() {
-    	return type == null || type.equals(CollectionType.USER.toString()) 
-			? WhatTheDuck.userCollection 
-			: WhatTheDuck.coaCollection; 
+    	return type == null || type.equals(CollectionType.USER.toString())
+			? WhatTheDuck.userCollection
+			: WhatTheDuck.coaCollection;
     }
 
 	protected void setNavigationCountry(String countryFullName, String selectedCountry) {
