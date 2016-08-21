e015abd069fa0ca0ef32e4d665e60ae099dbf484
Bruno Perel <brunoperel@gmail.com>
2016-08-21 22:28:13 +0200

    Fix wrong issue condition images shown when scrolling in the issue list

--- app/src/main/java/net/ducksmanager/whattheduck/IssueAdapter.java
+++ app/src/main/java/net/ducksmanager/whattheduck/IssueAdapter.java
@@ -1,7 +1,5 @@
 package net.ducksmanager.whattheduck;
 
-import java.util.ArrayList;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class IssueAdapter extends ArrayAdapter<Issue> {
 
@@ -32,9 +32,13 @@ public class IssueAdapter extends ArrayAdapter<Issue> {
             ImageView imageCondition = (ImageView) v.findViewById(R.id.issuecondition);
             if (issueNumber != null)
                 issueNumber.setText(i.getIssueNumber());
-            if (imageCondition != null && i.getIssueCondition() != null) {
-                int resourceId = Issue.issueConditionToResourceId(i.getIssueCondition());
-                imageCondition.setImageResource(resourceId);
+            if (imageCondition != null) {
+                if (i.getIssueCondition() != null) {
+                    int resourceId = Issue.issueConditionToResourceId(i.getIssueCondition());
+                    imageCondition.setImageResource(resourceId);
+                } else {
+                    imageCondition.setImageResource(android.R.color.transparent);
+                }
             }
         }
         return v;
