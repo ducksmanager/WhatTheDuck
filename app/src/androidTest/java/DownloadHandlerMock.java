5aeb5424f0d268a4a4a1a8e6da725b942e356b8e
Bruno Perel <brunoperel@gmail.com>
2018-04-07 00:22:04 +0200

    Mock covers Remove dependency to mockwebserverplus
    (cherry picked from commit 13509ee)

--- app/src/androidTest/java/DownloadHandlerMock.java
+++ app/src/androidTest/java/DownloadHandlerMock.java
@@ -1,14 +1,15 @@
 import android.net.UrlQuerySanitizer;
 
-import com.orhanobut.mockwebserverplus.Fixture;
-
 import net.ducksmanager.whattheduck.WhatTheDuck;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.io.InputStream;
 
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.RecordedRequest;
+import okio.Buffer;
+import okio.Okio;
 
 
 public class DownloadHandlerMock {
@@ -18,8 +19,8 @@ public class DownloadHandlerMock {
 
         @Override
         public MockResponse dispatch(RecordedRequest request) {
-            System.out.println("Mocking " + request.getRequestUrl());
-            if (request.getRequestUrl().url().toString().contains("/dm-server/")) {
+            System.out.println("Mocking " + request.getPath());
+            if (request.getPath().contains("/dm-server/")) {
                 return dispatchForDmServer(request);
             }
             else {
@@ -28,35 +29,73 @@ public class DownloadHandlerMock {
         }
 
         private MockResponse dispatchForDm(RecordedRequest request) {
-            if (request.getRequestUrl().url().toString().endsWith(WhatTheDuck.DUCKSMANAGER_PAGE_WITH_REMOTE_URL)) {
-                return new MockResponse().setBody(WtdTest.mockServer.url("/dm-server/"));
+            if (request.getPath().endsWith(WhatTheDuck.DUCKSMANAGER_PAGE_WITH_REMOTE_URL)) {
+                return new MockResponse().setBody(WtdTest.mockServer.url("/dm-server/").toString());
             }
             return new MockResponse().setStatus("404");
         }
 
         private MockResponse dispatchForDmServer(RecordedRequest request) {
-            UrlQuerySanitizer sanitizer = new UrlQuerySanitizer(request.getRequestUrl().url().toString());
+            UrlQuerySanitizer sanitizer = new UrlQuerySanitizer(request.getPath());
             String username = sanitizer.getValue("pseudo_user");
             if (username == null) {
-                if (request.getRequestUrl().pathSegments().contains("publications")) {
-                    return new MockResponse().setBody(Fixture.parseFrom("dm-server/publications").body);
+                String[] parts = request.getPath().split("/");
+                if (request.getPath().contains("publications")) {
+                    return new MockResponse().setBody(getJsonFixture("dm-server/publications"));
                 }
-                if (request.getRequestUrl().pathSegments().contains("issues")) {
-                    return new MockResponse().setBody(Fixture.parseFrom("dm-server/issues").body);
+                if (request.getPath().contains("issues")) {
+                    return new MockResponse().setBody(getJsonFixture("dm-server/issues"));
                 }
-                if (request.getRequestUrl().pathSegments().containsAll(Arrays.asList("cover-id", "search"))) {
-                    return new MockResponse().setBody(Fixture.parseFrom("dm-server/cover-search").body);
+                if (request.getPath().contains("cover-id/search")) {
+                    return new MockResponse().setBody(getJsonFixture("dm-server/cover-search"));
                 }
+                if (request.getPath().contains("cover-id/download")) {
+                    return new MockResponse().setBody(getImageFixture("covers/" + parts[parts.length-1]));
+                }
+
                 return new MockResponse().setStatus("500");
             }
             switch (username) {
                 case TEST_USER:
                     if (sanitizer.getValue("mdp_user").equals(WhatTheDuck.toSHA1(TEST_PASS))) {
-                        return new MockResponse().setBody(Fixture.parseFrom("dm/collection").body);
+                        return new MockResponse().setBody(getJsonFixture("dm/collection"));
                     }
                     break;
             }
             return new MockResponse().setBody("0");
         }
     };
+
+    private static String getJsonFixture(String name) {
+        String path="fixtures/" + name + ".json";
+
+        InputStream inputStream = openPathAsStream(path);
+        return convertStreamToString(inputStream);
+    }
+
+    private static Buffer getImageFixture(String name) {
+        Buffer buffer = new Buffer();
+        try {
+            buffer.writeAll(Okio.source(openPathAsStream("fixtures/" + name + ".jpg")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+
+    private static InputStream openPathAsStream(String path) {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = loader.getResourceAsStream(path);
+
+        if (inputStream == null) {
+            throw new IllegalStateException("Invalid path: " + path);
+        }
+
+        return inputStream;
+    }
+
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
 }
