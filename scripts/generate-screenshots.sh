7ba201b678c428b49fd181be5de301e8104e289d
Bruno Perel <brunoperel@gmail.com>
2018-04-07 19:42:26 +0200

    Stop screenshot retrieval script whenever an error occurs

--- scripts/generate-screenshots.sh
+++ scripts/generate-screenshots.sh
@@ -2,17 +2,17 @@
 dirname=showcase
 remote_dir=/data/data/net.ducksmanager.whattheduck/files/test-screenshots/${dirname}
 
-rm -rf ${dirname}
-adb shell rm -r ${remote_dir}
-adb shell am force-stop net.ducksmanager.whattheduck.test
-adb shell am instrument -w -r -e debug false -e class ScreenshotTest net.ducksmanager.whattheduck.test/android.support.test.runner.AndroidJUnitRunner
-adb pull ${remote_dir} `pwd`
-for locale in fr en sv; do
-    convert \
-        "$dirname/$locale/Cover search result.png" \
-        \( -clone 0 -fill white -colorize 100 -fill black \
-        -draw "polygon 120,375 350,375 350,610 120,610" \
-        -alpha off -write mpr:mask +delete \) \
-        -mask mpr:mask -blur 0x5 +mask "$dirname/$locale/Cover search result_blurred.png" \
-    && rm "$dirname/$locale/Cover search result.png"
-done
+adb shell am force-stop net.ducksmanager.whattheduck.test && \
+adb shell rm -r ${remote_dir} && \
+adb shell am instrument -w -r -e debug false -e class ScreenshotTest net.ducksmanager.whattheduck.test/android.support.test.runner.AndroidJUnitRunner && \
+adb pull ${remote_dir} `pwd` && (
+    for locale in fr en sv; do
+        convert \
+            "$dirname/$locale/Cover search result.png" \
+            \( -clone 0 -fill white -colorize 100 -fill black \
+            -draw "polygon 120,375 350,375 350,610 120,610" \
+            -alpha off -write mpr:mask +delete \) \
+            -mask mpr:mask -blur 0x5 +mask "$dirname/$locale/Cover search result_blurred.png" \
+        && rm "$dirname/$locale/Cover search result.png"
+    done
+)
