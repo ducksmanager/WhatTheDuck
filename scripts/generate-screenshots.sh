b98031ac3707414354b159d065727901f1fac9f8
Bruno Perel <brunoperel@gmail.com>
2018-04-28 13:10:03 +0200

    Generate a picture from cover search results screenshot + add issue screenshot combined

--- scripts/generate-screenshots.sh
+++ scripts/generate-screenshots.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 dirname=showcase
 remote_dir=/data/data/net.ducksmanager.whattheduck/files/test-screenshots/${dirname}
+test_app_name=net.ducksmanager.whattheduck.test
 
-adb shell am force-stop net.ducksmanager.whattheduck.test && \
+adb shell am force-stop ${test_app_name} && \
 adb shell rm -r ${remote_dir} && \
-adb shell am instrument -w -r -e debug false -e class ScreenshotTest net.ducksmanager.whattheduck.test/android.support.test.runner.AndroidJUnitRunner && \
+adb shell am instrument -w -r -e debug false -e class ScreenshotTest ${test_app_name}/android.support.test.runner.AndroidJUnitRunner && \
 adb pull ${remote_dir} `pwd` && (
     for locale in fr en sv; do
         convert \
@@ -13,6 +14,7 @@ adb pull ${remote_dir} `pwd` && (
             -draw "polygon 120,375 350,375 350,610 120,610" \
             -alpha off -write mpr:mask +delete \) \
             -mask mpr:mask -blur 0x5 +mask "$dirname/$locale/Cover search result_blurred.png" \
-        && rm "$dirname/$locale/Cover search result.png"
+        && rm "$dirname/$locale/Cover search result.png" \
+        && composite -gravity center "$dirname/$locale/Cover search result - add issue.png"  "$dirname/$locale/Cover search result_blurred.png" "$dirname/$locale/Cover search result_blurred - with add issue.png"
     done
 )
