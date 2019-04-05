22e59c3ba133921ecb3d48dbd38a07b30df68a0e
Bruno Perel <brunoperel@gmail.com>
2019-04-05 18:56:56 +0200

    Cleanup script

--- scripts/generate-screenshots.sh
+++ scripts/generate-screenshots.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 blurPicture() {
     source=$1
     destination=$2
@@ -19,44 +21,45 @@ remote_app_dir=/data/local/tmp/${test_app_name}
 remote_screenshot_dir=/data/data/net.ducksmanager.whattheduck/files/test-screenshots/${showcasedir}
 shellAsApp="adb shell run-as net.ducksmanager.whattheduck"
 
-rm -rf /sdcard/$showcasedir && \
-$shellAsApp rm -r ${remote_screenshot_dir}
-adb shell am force-stop ${test_app_name} && \
-./gradlew :app:assembleDebugAndroidTest && \
-adb push app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk ${remote_app_dir} && \
-adb shell pm install -t -r ${remote_app_dir} && \
+rm -rf /sdcard/$showcasedir
+$shellAsApp rm -rf ${remote_screenshot_dir}
+adb shell am force-stop ${test_app_name}
+./gradlew :app:assembleDebugAndroidTest
+adb push app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk ${remote_app_dir}
+adb shell pm install -t -r ${remote_app_dir}
 \
-adb shell am instrument -w -r -e debug false -e class ListTest,CoverSearchTest ${test_app_name}/android.support.test.runner.AndroidJUnitRunner && \
-$shellAsApp cp -r $remote_screenshot_dir /sdcard && \
-adb pull /sdcard/$showcasedir `pwd` && (
-    for locale in fr en sv; do
-        blurPicture \
-            "$showcasedir/$locale/Cover search result.png" \
-            "$showcasedir/$locale/Cover search result_blurred.png" \
-            "560,860 700,860 700,970 560,970" && \
-        blurPicture \
-            "$showcasedir/$locale/Cover search result_blurred.png" \
-            "$showcasedir/$locale/Cover search result_blurred.png" \
-            "400,870 520,870 520,970 400,970" && \
-        blurPicture \
-            "$showcasedir/$locale/Cover search result_blurred.png" \
-            "$showcasedir/$locale/Cover search result_blurred.png" \
-            "0,900 50,900 50,950 0,950" && \
-        blurPicture \
-            "$showcasedir/$locale/Cover search result_blurred.png" \
-            "$showcasedir/$locale/Cover search result_blurred.png" \
-            "1040,900 1078,900 1078,950 1040,950" && \
-        rm "$showcasedir/$locale/Cover search result.png" && \
-        blurPicture \
-            "$showcasedir/$locale/Collection - Issue list - edge view.png" \
-            "$showcasedir/$locale/Collection - Issue list - edge view_blurred.png" \
-            "470,430 690,430 690,620 470,620" && \
-        blurPicture \
-            "$showcasedir/$locale/Collection - Issue list - edge view_blurred.png" \
-            "$showcasedir/$locale/Collection - Issue list - edge view_blurred.png" \
-            "360,910 480,910 480,1020 360,1020" && \
-        rm "$showcasedir/$locale/Collection - Issue list - edge view.png" && \
-        composite -gravity center "$showcasedir/$locale/Cover search result - add issue.png"  "$showcasedir/$locale/Cover search result_blurred.png" "$showcasedir/$locale/Cover search result_blurred - with add issue.png" && \
-        rm "$showcasedir/$locale/Cover search result - add issue.png"
-    done
-)
+
+adb shell am instrument -w -r -e debug false -e class ListTest,CoverSearchTest ${test_app_name}/android.support.test.runner.AndroidJUnitRunner
+$shellAsApp cp -r $remote_screenshot_dir /sdcard
+adb pull /sdcard/$showcasedir `pwd`
+
+for locale in fr en sv; do
+    blurPicture \
+        "$showcasedir/$locale/Cover search result.png" \
+        "$showcasedir/$locale/Cover search result_blurred.png" \
+        "560,860 700,860 700,970 560,970" && \
+    blurPicture \
+        "$showcasedir/$locale/Cover search result_blurred.png" \
+        "$showcasedir/$locale/Cover search result_blurred.png" \
+        "400,870 520,870 520,970 400,970" && \
+    blurPicture \
+        "$showcasedir/$locale/Cover search result_blurred.png" \
+        "$showcasedir/$locale/Cover search result_blurred.png" \
+        "0,900 50,900 50,950 0,950" && \
+    blurPicture \
+        "$showcasedir/$locale/Cover search result_blurred.png" \
+        "$showcasedir/$locale/Cover search result_blurred.png" \
+        "1040,900 1078,900 1078,950 1040,950" && \
+    rm "$showcasedir/$locale/Cover search result.png" && \
+    blurPicture \
+        "$showcasedir/$locale/Collection - Issue list - edge view.png" \
+        "$showcasedir/$locale/Collection - Issue list - edge view_blurred.png" \
+        "470,430 690,430 690,620 470,620" && \
+    blurPicture \
+        "$showcasedir/$locale/Collection - Issue list - edge view_blurred.png" \
+        "$showcasedir/$locale/Collection - Issue list - edge view_blurred.png" \
+        "360,910 480,910 480,1020 360,1020" && \
+    rm "$showcasedir/$locale/Collection - Issue list - edge view.png" && \
+    composite -gravity center "$showcasedir/$locale/Cover search result - add issue.png"  "$showcasedir/$locale/Cover search result_blurred.png" "$showcasedir/$locale/Cover search result_blurred - with add issue.png" && \
+    rm "$showcasedir/$locale/Cover search result - add issue.png"
+done
