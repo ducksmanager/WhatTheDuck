c8a3c0426229b2b0af648cc47f538b9e6a93aff9
Bruno Perel <brunoperel@gmail.com>
2022-08-04 18:26:24 +0200

    Fix crash on update from 2.14 to 2.15

--- app/src/main/java/net/ducksmanager/persistence/AppDatabase.kt
+++ app/src/main/java/net/ducksmanager/persistence/AppDatabase.kt
@@ -89,7 +89,7 @@ abstract class AppDatabase : RoomDatabase() {
         }
         val MIGRATION_16_17: Migration = object : Migration(16, 17) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE issues ADD `isToRead` INTEGER NOT NULL")
+                database.execSQL("ALTER TABLE issues ADD `isToRead` INTEGER NOT NULL DEFAULT 0")
             }
         }
     }
